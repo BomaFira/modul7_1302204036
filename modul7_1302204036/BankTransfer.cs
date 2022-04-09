fb3e99a7a9b61711f540255cdb549bd098e835f3
BomaFira <Cluezee@student.telkomuniversity.ac.id>
2022-04-09 17:59:29 +0700

    Add project files.

---
+++ modul7_1302204036/BankTransfer.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul7_1302204036
+{
+    internal class BankTransferConfig
+    {
+        public dynamic ReadJson()
+        {
+            string jString = File.ReadAllText("D:/Tel U/Semester 4/KPL/pratikum_kpl/Modul7_1302204036/modul7_1302204036/modul7_1302204036/banktransferconfig.json");
+            dynamic data = JsonConvert.DeserializeObject(jString);
+            return data;
+        }
+    }
+}
---
+++ modul7_1302204036/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul7_1302204036
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankTransferConfig bankTransferConfig = new BankTransferConfig();
+            dynamic bankConfig = bankTransferConfig.ReadJson();
+
+            if (bankConfig.lang == "en")
+            {
+                Console.WriteLine("Please insert the amount of money to transfer:");
+            }
+            else if (bankConfig.lang == "id")
+            {
+                Console.WriteLine("Masukkan jumlah uang yang akan di-transfer:");
+            }
+
+            int jumlah = int.Parse(Console.ReadLine());
+            int fee;
+            if (jumlah <= (int)bankConfig.transfer.threshold)
+            {
+                fee = bankConfig.transfer.low_fee;
+            }
+            else
+            {
+                fee = bankConfig.transfer.high_fee;
+            }
+
+            if (bankConfig.lang == "en")
+            {
+                Console.WriteLine("Transfer fee = " + fee);
+                Console.WriteLine("Total amount = " + (fee + jumlah) + "\n");
+                Console.WriteLine("Select Transfer Method");
+            }
+            else if (bankConfig.lang == "id")
+            {
+                Console.WriteLine("Biaya Transfer = " + fee);
+                Console.WriteLine("Total Biaya = " + (fee + jumlah) + "\n");
+                Console.WriteLine("Pilih Metode Transfer");
+            }
+            int index = 0;
+            foreach (var method in bankConfig.methods)
+            {
+                index++;
+                Console.WriteLine(index + ". " + method);
+            }
+            Console.WriteLine();
+            Console.ReadLine();
+
+            string konfirmasi;
+            if (bankConfig.lang == "en")
+            {
+                Console.WriteLine("Please type '" + bankConfig.confirmation.en + "' to confirm the transaction:");
+                konfirmasi = Console.ReadLine();
+                if (konfirmasi == (string)bankConfig.confirmation.en)
+                {
+                    Console.WriteLine("The Transfer Is Completed");
+                }
+                else
+                {
+                    Console.WriteLine("Transfer Is Cancelled");
+                }
+            }
+            else if (bankConfig.lang == "id")
+            {
+                Console.WriteLine("Ketik '" + bankConfig.confirmation.id + "' untuk mengkonfirmasi transaksi anda:");
+                konfirmasi = Console.ReadLine();
+                if (konfirmasi == (string)bankConfig.confirmation.id)
+                {
+                    Console.WriteLine("Proses Transfer BERHASIL");
+                }
+                else
+                {
+                    Console.WriteLine("Transfer DIBATALKAN");
+                }
+            }
+        }
+    }
+}
